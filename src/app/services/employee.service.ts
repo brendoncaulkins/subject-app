39c2bb82efebb63734bb80e39dc6806486cf12d4
brendoncaulkins <brendon.caulkins@excella.com>
2018-07-10 17:51:54 -0400

    Switch to local backend

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -7,47 +7,27 @@ import { Employee } from '../models/employee.model'
 
 @Injectable()
 export class EmployeeService {
-  private databaseName = 'subject-app'
   private tableName = 'employees'
-  private dbAuthKey = 'MqiOri2zgRMoZAaVocEjZznpj56u4TP2OtzynXuw'
 
   constructor(private httpClient: HttpClient) {}
 
   all(): Observable<Employee[]> {
-    return this.httpClient.get<Employee[]>(this.buildUrl(), {
-      params: this.buildParams(),
-    })
+    return this.httpClient.get<Employee[]>(this.buildUrl())
   }
 
   get(id: string): Observable<Employee> {
-    return this.httpClient.get<Employee>(`${this.buildUrl()}/${id}`, {
-      params: this.buildParams(),
-    })
+    return this.httpClient.get<Employee>(`${this.buildUrl()}/${id}`)
   }
 
   save(employee: Employee): Observable<Employee> {
     if (employee.id) {
-      return this.httpClient.put<Employee>(
-        `${this.buildUrl()}/${employee.id}`,
-        employee,
-        {
-          params: this.buildParams(),
-        }
-      )
+      return this.httpClient.put<Employee>(`${this.buildUrl()}/${employee.id}`, employee)
     } else {
-      return this.httpClient.post<Employee>(`${this.buildUrl()}/`, employee, {
-        params: this.buildParams(),
-      })
+      return this.httpClient.post<Employee>(`${this.buildUrl()}/`, employee)
     }
   }
 
   private buildUrl() {
-    return `https://${this.databaseName}.firebaseio.com/${this.tableName}.json`
-  }
-
-  private buildParams() {
-    return {
-      auth: this.dbAuthKey,
-    }
+    return `http://localhost:8080/${this.tableName}`
   }
 }
