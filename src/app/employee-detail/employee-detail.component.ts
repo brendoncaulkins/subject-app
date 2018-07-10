7c4050423497b9f738f5e503c2a4ec37f5d0ce8d
brendoncaulkins <brendon.caulkins@excella.com>
2018-07-10 17:52:03 -0400

    Basic employee details view

--- src/app/employee-detail/employee-detail.component.ts
+++ src/app/employee-detail/employee-detail.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+
+import { Subscription } from 'rxjs'
+
+import { Employee } from '../models/employee.model'
+import { EmployeeService } from '../services/employee.service'
 
 @Component({
   selector: 'app-employee-detail',
   templateUrl: './employee-detail.component.html',
-  styles: []
+  styles: [],
 })
-export class EmployeeDetailComponent implements OnInit {
+export class EmployeeDetailComponent implements OnInit, OnDestroy {
+  employee: Employee
+  employeeSubscription: Subscription
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private employeeService: EmployeeService, private route: ActivatedRoute) {
+    this.employee = new Employee()
   }
 
+  ngOnInit() {
+    this.employeeSubscription = this.employeeService
+      .get(this.route.snapshot.params['id'])
+      .subscribe(employee => (this.employee = new Employee().fromJSON(employee)))
+  }
+
+  ngOnDestroy() {
+    this.employeeSubscription.unsubscribe()
+  }
 }
