1fcfd652c3e212c7d3a5950dacb3b6449e5ae557
brendoncaulkins <brendon.caulkins@excella.com>
2018-07-10 18:20:08 -0400

    Basic employee list

--- src/app/employee-list/employee-list-card/employee-list-card.component.ts
+++ src/app/employee-list/employee-list-card/employee-list-card.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
+
+import { Employee } from '../../models/employee.model'
 
 @Component({
   selector: 'app-employee-list-card',
   templateUrl: './employee-list-card.component.html',
-  styles: []
+  styleUrls: ['./employee-list-card.component.css'],
 })
 export class EmployeeListCardComponent implements OnInit {
+  @Input() employee: Employee
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  edit(id: string) {
+    this.router.navigate(['employee', 'edit', id])
   }
-
 }
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+
+import { BehaviorSubject } from 'rxjs'
+
+import { Employee } from '../models/employee.model'
+import { EmployeeService } from '../services/employee.service'
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
-  styles: []
+  styles: [],
 })
 export class EmployeeListComponent implements OnInit {
+  employeeList: BehaviorSubject<Employee[]>
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private employeesService: EmployeeService) {
+    this.employeeList = new BehaviorSubject<Employee[]>([])
   }
 
+  ngOnInit() {
+    this.employeesService
+      .all()
+      .subscribe(
+        list => this.employeeList.next(list),
+        error => this.employeeList.error(error)
+      )
+  }
 }
